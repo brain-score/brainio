16c79119079efd0a9af571dd3cac6459e8dcea7f
Jonathan Prescott-Roy <jjpr@mit.edu>
2021-06-25 11:13:57 -0400

    numpy 1.21.0 came out 3 days ago, and appears to have some bugs.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ requirements = [
     "boto3",
     "tqdm",
     "Pillow",
-    "numpy>=1.16.5",
+    "numpy>=1.16.5, !=1.21.0",
     "pandas>=1.2.0",
     "xarray>=0.17.0", # a bug introduced in 0.16.2 causes align to handle MultiIndex wrong
     # test_requirements
