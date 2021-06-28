881f3d287f7a5c75b2e8c421f4f282bf5bad8687
jjpr-mit <jjpr@mit.edu>
2021-06-28 14:36:13 -0400

    Putting lookup.csv in tests.  On pip install, top level would translate to site-packages, but it's not there. (#2)

--- brainio/entrypoint.py
+++ brainio/entrypoint.py
@@ -5,7 +5,7 @@ import pandas as pd
 
 _logger = logging.getLogger(__name__)
 
-path = Path(__file__).parent.parent / "lookup.csv"
+path = Path(__file__).parent.parent / "tests" / "lookup.csv"
 _logger.debug(f"Loading lookup from {path}")
 print(f"Loading lookup from {path}")  # print because logging usually isn't set up at this point during import
 brainio_test = pd.read_csv(path)
