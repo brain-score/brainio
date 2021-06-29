4208536ead3f600c5658eb90d958e1887afaed0f
Jonathan Prescott-Roy <jjpr@mit.edu>
2021-06-29 14:31:27 -0400

    Making sure entry points are set up properly.  Making the tests entry point an "extra".

--- brainio/entrypoint.py
+++ brainio/entrypoint.py
@@ -5,8 +5,9 @@ import pandas as pd
 
 _logger = logging.getLogger(__name__)
 
-path = Path(__file__).parent.parent / "tests" / "lookup.csv"
-_logger.debug(f"Loading lookup from {path}")
-print(f"Loading lookup from {path}")  # print because logging usually isn't set up at this point during import
-brainio_test = pd.read_csv(path)
+def brainio_test():
+    path = Path(__file__).parent / "lookup.csv"
+    _logger.debug(f"Loading lookup from {path}")
+    print(f"Loading lookup from {path}")  # print because logging usually isn't set up at this point during import
+    return pd.read_csv(path)
 
