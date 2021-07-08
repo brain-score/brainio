f9b08f3436eb6d1de9fc3a469f7a9de3dc35a22e
Jonathan Prescott-Roy <jjpr@mit.edu>
2021-07-08 14:38:32 -0400

    Also affected by the pandas 1.3.0 bug.

--- setup.py
+++ setup.py
@@ -13,8 +13,8 @@ requirements = [
     "Pillow",
     "entrypoints",
     "numpy>=1.16.5, !=1.21.0",
-    "pandas>=1.2.0",
-    "xarray>=0.17.0", # a bug introduced in 0.16.2 causes align to handle MultiIndex wrong
+    "pandas>=1.2.0, !=1.3.0",
+    "xarray>=0.17.0",
     "netcdf4",
 ]
 
