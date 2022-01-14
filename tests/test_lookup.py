a73e02005e95a0e9767265b10be486e38931e3d6
Jonathan Prescott-Roy <jjpr@mit.edu>
2022-01-14 18:35:29 -0500

    Pruned unnecessary tests.  Made catalog brainio_test with 4 stimulus sets and 4 data assemblies, and catalog brainio_test with 3 stimulus sets and 3 data assemblies, with one overlap to test handling of duplicates.  Also added some basic missing tests.

--- tests/test_lookup.py
+++ tests/test_lookup.py
@@ -19,38 +19,26 @@ from brainio.assemblies import DataAssembly, get_levels, gather_indexes, is_fast
         'dicarlo.MajajHong2015',
         'dicarlo.MajajHong2015.private',
         'dicarlo.MajajHong2015.public',
-        'dicarlo.MajajHong2015.temporal',
-        'dicarlo.MajajHong2015.temporal.private',
-        'dicarlo.MajajHong2015.temporal.public',
-        'dicarlo.MajajHong2015.temporal-10ms',
-        'gallant.David2004',
         'tolias.Cadena2017',
-        'movshon.FreemanZiemba2013',
-        'movshon.FreemanZiemba2013.private',
-        'movshon.FreemanZiemba2013.public',
-        'dicarlo.Rajalingham2018.public', 'dicarlo.Rajalingham2018.private',
-        'dicarlo.Kar2019',
-        'dicarlo.Kar2018hvm',
-        'dicarlo.Kar2018cocogray',
-        'klab.Zhang2018search_obj_array',
-        'aru.Kuzovkin2018',
-        'dicarlo.Rajalingham2020',
-        'dicarlo.SanghaviMurty2020',
-        'dicarlo.SanghaviJozwik2020',
-        'dicarlo.Sanghavi2020',
-        'dicarlo.SanghaviMurty2020THINGS1',
-        'dicarlo.SanghaviMurty2020THINGS2',
-        'aru.Kuzovkin2018',
-        'dicarlo.Seibert2019',
-        'aru.Cichy2019',
-        'dicarlo.Rust2012.single',
-        'dicarlo.Rust2012.array',
         'dicarlo.BashivanKar2019.naturalistic',
         'dicarlo.BashivanKar2019.synthetic',
 ))
 def test_list_assembly(assembly):
     l = brainio.list_assemblies()
     assert assembly in l
+
+
+@pytest.mark.parametrize('stimulus_set', (
+        'dicarlo.hvm',
+        'dicarlo.hvm-public',
+        'dicarlo.hvm-private',
+        'tolias.Cadena2017',
+        'dicarlo.BashivanKar2019.naturalistic',
+        'dicarlo.BashivanKar2019.synthetic'
+))
+def test_list_stimulus_set(stimulus_set):
+    l = brainio.list_stimulus_sets()
+    assert stimulus_set in l
 
 
 def test_lookup_stim():
@@ -62,7 +50,7 @@ def test_lookup_stim():
     assert stim_zip['identifier'] == "dicarlo.hvm"
     assert stim_zip['location_type'] == "S3"
     hvm_s3_zip_url = "https://brainio.dicarlo.s3.amazonaws.com/image_dicarlo_hvm.zip"
-    assert stim_csv['location'] == hvm_s3_zip_url
+    assert stim_zip['location'] == hvm_s3_zip_url
 
 
 def test_lookup_assy():
@@ -71,14 +59,6 @@ def test_lookup_assy():
     assert assy['location_type'] == "S3"
     hvm_s3_url = "https://brainio.dicarlo.s3.amazonaws.com/assy_dicarlo_MajajHong2015_public.nc"
     assert assy['location'] == hvm_s3_url
-
-
-def test_lookup_stim():
-    csv, zip = brainio.lookup.lookup_stimulus_set("dicarlo.hvm")
-    assert csv['identifier'] == "dicarlo.hvm"
-    assert csv['location_type'] == "S3"
-    hvm_s3_url = "https://brainio.dicarlo.s3.amazonaws.com/image_dicarlo_hvm.csv"
-    assert csv['location'] == hvm_s3_url
 
 
 def test_lookup_bad_name():
@@ -94,27 +74,33 @@ def test_catalogs():
     dfs = brainio.lookup.get_lookups()
     assert dfs["brainio_test"].attrs[brainio.lookup.CATALOG_PATH_KEY].endswith(".csv")
     assert dfs["brainio_test2"].attrs[brainio.lookup.CATALOG_PATH_KEY].endswith(".csv")
-    assert len(dfs["brainio_test2"]) == 5
+    assert len(dfs["brainio_test"]) == 12
+    assert len(dfs["brainio_test2"]) == 9
     concat = brainio.lookup.data()
     assert len(concat) == len(dfs["brainio_test"]) + len(dfs["brainio_test2"])
 
 
 def test_duplicates():
     all_lookups = brainio.lookup.data()
-    match_hvm = all_lookups['identifier'] == "dicarlo.hvm"
-    assert np.count_nonzero(match_hvm) == 4
     match_stim = all_lookups['lookup_type'] == brainio.lookup.TYPE_STIMULUS_SET
     match_csv = all_lookups.apply(brainio.lookup._is_csv_lookup, axis=1)
     match_zip = all_lookups.apply(brainio.lookup._is_zip_lookup, axis=1)
+    match_assy = all_lookups['lookup_type'] == brainio.lookup.TYPE_ASSEMBLY
+
+    match_hvm = all_lookups['identifier'] == "dicarlo.hvm"
+    assert np.count_nonzero(match_hvm) == 4
     assert len(all_lookups[match_hvm & match_stim & match_csv]) == 2
     assert len(all_lookups[match_hvm & match_stim & match_zip]) == 2
     match_mh15 = all_lookups['identifier'] == "dicarlo.MajajHong2015"
     match_mh15_pub = all_lookups['identifier'] == "dicarlo.MajajHong2015.public"
     match_mh15_pvt = all_lookups['identifier'] == "dicarlo.MajajHong2015.private"
-    match_assy = all_lookups['lookup_type'] == brainio.lookup.TYPE_ASSEMBLY
     assert len(all_lookups[match_mh15 & match_assy]) == 2
-    assert len(all_lookups[match_mh15_pub & match_assy]) == 2
-    assert len(all_lookups[match_mh15_pvt & match_assy]) == 2
+    assert len(all_lookups[match_mh15_pub & match_assy]) == 1
+    assert len(all_lookups[match_mh15_pvt & match_assy]) == 1
+    match_c17 = all_lookups['identifier'] == "tolias.Cadena2017"
+    assert len(all_lookups[match_c17 & match_stim & match_csv]) == 1
+    assert len(all_lookups[match_c17 & match_stim & match_zip]) == 1
+    assert len(all_lookups[match_c17 & match_assy]) == 1
 
 
 
