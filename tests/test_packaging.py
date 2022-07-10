9b5f630a7f35a717a13ee4d4b4d6e12a5a9075c1
Jonathan Prescott-Roy <jjpr@mit.edu>
2022-07-09 20:58:29 -0400

    The append() function in packaging is adding unneeded columns to the catalog CSV.

--- tests/test_packaging.py
+++ tests/test_packaging.py
@@ -116,6 +116,9 @@ def test_package_stimulus_set(test_stimulus_set_identifier, test_catalog_identif
 @pytest.mark.private_access
 def test_package_data_assembly(test_stimulus_set_identifier, test_catalog_identifier, brainio_home,
                                restore_this_file, restore_catalog):
+    catalog = lookup.get_catalog(test_catalog_identifier)
+    assert 'lookup_source' not in catalog
+    assert 'source_catalog' not in catalog
     stimulus_set = StimulusSet(make_stimulus_set_df())
     stimulus_set.stimulus_paths = {row["stimulus_id"]: Path(__file__).parent / f'images/{row["filename"]}' for _, row in stimulus_set.iterrows()}
     del stimulus_set["filename"]
@@ -140,6 +143,9 @@ def test_package_data_assembly(test_stimulus_set_identifier, test_catalog_identi
     gotten = brainio.get_assembly(identifier)
     assert gotten is not None
     assert gotten.shape == (6, 3, 1)
+    catalog = lookup.get_catalog(test_catalog_identifier)
+    assert 'lookup_source' not in catalog
+    assert 'source_catalog' not in catalog
 
 
 @pytest.mark.private_access
