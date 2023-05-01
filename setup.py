9bcf258b5514bac72b5e96efe8ab2d93f2cf07e9
Martin Schrimpf <mschrimpf@users.noreply.github.com>
2023-04-30 23:53:44 -0400

    restrict to pandas 1 for now
    python>3.7 builds fail otherwise: https://app.travis-ci.com/github/brain-score/brainio/builds/262688391

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ requirements = [
     "Pillow",
     "entrypoints",
     "numpy",
-    "pandas",
+    "pandas<2",
     "xarray<2022.6",  # groupby bug was introduced in index refactor: https://github.com/pydata/xarray/issues/6836
     "netcdf4!=1.6.0",  # https://github.com/Unidata/netcdf4-python/issues/1175,
 ]
