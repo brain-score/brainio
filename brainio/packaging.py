dc42188f2cc26c65e18282b553d3bdbf3cc5218b
Mike Ferguson <mef8dd@virginia.edu>
2024-03-12 14:48:29 -0400

    netCDF case type error fixed

--- brainio/packaging.py
+++ brainio/packaging.py
@@ -167,20 +167,22 @@ def package_stimulus_set(catalog_name, proto_stimulus_set, stimulus_set_identifi
     upload_to_s3(str(target_csv_path), bucket_name, target_s3_key=csv_file_name)
     upload_to_s3(str(target_zip_path), bucket_name, target_s3_key=zip_file_name)
     # link to csv and zip from same identifier. The csv however is the only one of the two rows with a class.
-    lookup.append(
-        catalog_identifier=catalog_name,
-        object_identifier=stimulus_set_identifier, cls='StimulusSet',
-        lookup_type=TYPE_STIMULUS_SET,
-        bucket_name=bucket_name, sha1=csv_sha1, s3_key=csv_file_name,
-        stimulus_set_identifier=None
-    )
-    lookup.append(
-        catalog_identifier=catalog_name,
-        object_identifier=stimulus_set_identifier, cls=None,
-        lookup_type=TYPE_STIMULUS_SET,
-        bucket_name=bucket_name, sha1=stimulus_zip_sha1, s3_key=zip_file_name,
-        stimulus_set_identifier=None
-    )
+
+    if catalog_name is not None:
+        lookup.append(
+            catalog_identifier=catalog_name,
+            object_identifier=stimulus_set_identifier, cls='StimulusSet',
+            lookup_type=TYPE_STIMULUS_SET,
+            bucket_name=bucket_name, sha1=csv_sha1, s3_key=csv_file_name,
+            stimulus_set_identifier=None
+        )
+        lookup.append(
+            catalog_identifier=catalog_name,
+            object_identifier=stimulus_set_identifier, cls=None,
+            lookup_type=TYPE_STIMULUS_SET,
+            bucket_name=bucket_name, sha1=stimulus_zip_sha1, s3_key=zip_file_name,
+            stimulus_set_identifier=None
+        )
     _logger.debug(f"stimulus set {stimulus_set_identifier} packaged")
 
 
@@ -247,8 +249,6 @@ def package_data_assembly(catalog_identifier, proto_data_assembly, assembly_iden
     assembly = assembly_class(proto_data_assembly)
     assembly.attrs['stimulus_set_identifier'] = stimulus_set_identifier
     assembly.validate()
-    assert stimulus_set_identifier in list_stimulus_sets(), \
-        f"StimulusSet {stimulus_set_identifier} not found in packaged stimulus sets"
 
     # identifiers
     assembly_store_identifier = "assy_" + assembly_identifier.replace(".", "_")
@@ -263,13 +263,15 @@ def package_data_assembly(catalog_identifier, proto_data_assembly, assembly_iden
             assert isinstance(ex, DataArray)
             netcdf_kf_sha1 = write_netcdf(ex, target_netcdf_path, append=True, group=k)
     object_properties = upload_to_s3(target_netcdf_path, bucket_name, s3_key)
-    lookup.append(
-        catalog_identifier=catalog_identifier,
-        object_identifier=assembly_identifier, stimulus_set_identifier=stimulus_set_identifier,
-        lookup_type=TYPE_ASSEMBLY,
-        bucket_name=bucket_name, sha1=netcdf_kf_sha1,
-        s3_key=s3_key, cls=assembly_class_name,
-    )
+
+    if catalog_identifier is not None:
+        lookup.append(
+            catalog_identifier=catalog_identifier,
+            object_identifier=assembly_identifier, stimulus_set_identifier=stimulus_set_identifier,
+            lookup_type=TYPE_ASSEMBLY,
+            bucket_name=bucket_name, sha1=netcdf_kf_sha1,
+            s3_key=s3_key, cls=assembly_class_name,
+        )
     _logger.debug(f"assembly {assembly_identifier} packaged: sha1={netcdf_kf_sha1}" +
                   # log version_id if it's there (not all buckets are versioned)
                   (f", version_id={object_properties['VersionId']}" if 'VersionId' in object_properties else ""))
